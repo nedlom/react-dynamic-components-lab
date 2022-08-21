a8b86b7d8d7f9b87e69e86c85445fd13c8d1fef1
Nick <nelom3@gmail.com>
2022-08-21 09:01:16 -0400

    commit tree c0af22144ed7e718c5b9579a4450feffbfb91694

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Comment from "./Comment"
 // are we missing an import?
 
 export default class BlogPost extends Component {
@@ -17,11 +18,11 @@ export default class BlogPost extends Component {
         Just like hopes springing high,<br/>
         Still I'll rise.<br/>
         -Maya Angelou<br/>
+        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
 
         {/* (remove the comment ticks and their surrounding brackets)
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
         */}
       </div>
     );
